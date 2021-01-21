a5ba41b81bd252ac4d51d21407a6c0d06017b658
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-01-21 21:52:10 +0300

    CoreBookcase

---
+++ CoreBookcase/CoreBookcase/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CoreBookcase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBookcase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book = new List<Books>()
+            {
+                new Books(){ID=1, BookName="Satranç", Author="Zweig"},
+                new Books(){ID=2, BookName="xxxx", Author="YYY"},
+                new Books(){ID=3, BookName="Yaşam", Author="kdfjdkf"},
+                new Books(){ID=4, BookName="İnsanlık", Author="Ray"},
+                new Books(){ID=5, BookName="Umut", Author="Cey"},
+            };
+            return View(book);
+        }
+    }
+}
---
+++ CoreBookcase/CoreBookcase/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBookcase.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+    }
+}
