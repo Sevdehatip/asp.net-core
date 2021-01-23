78888edb6c230d2ef3d693294dde572a6a44ed9d
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-01-23 23:53:16 +0300

    ViewComponents

---
+++ CoreBookcase/CoreBookcase/ViewComponents/NewBooks.cs
@@ -0,0 +1,32 @@
+﻿using CoreBookcase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBookcase.ViewComponents
+{
+    public class NewBooks:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var bookList = new List<Books>
+            {
+                new Books
+                {
+                    ID=6,
+                    BookName="Korku",
+                    Author="Zweig"
+                },
+                new Books
+                {
+                    ID=7,
+                    BookName="Anna",
+                    Author="Tolstoy"
+                }
+            };
+            return View(bookList);
+        }
+    }
+}
