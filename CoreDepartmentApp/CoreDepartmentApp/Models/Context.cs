6c482a62eddba2feb888e44a30b7440098cf3291
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-01-29 22:56:55 +0300

    model classes

---
+++ CoreDepartmentApp/CoreDepartmentApp/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDepartmentApp.Models
+{
+    public class Context:DbContext
+    {
+        public DbSet<Department> departments { get; set; }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
---
+++ CoreDepartmentApp/CoreDepartmentApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDepartmentApp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string city { get; set; }
+
+    }
+}
