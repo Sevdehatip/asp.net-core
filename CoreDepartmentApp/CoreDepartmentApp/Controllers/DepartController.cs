70916d68483d5f3b11f2bb9468d993aae0c5ae95
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-02-02 21:35:56 +0300

    Add, Remove

--- CoreDepartmentApp/CoreDepartmentApp/Controllers/DepartController.cs
+++ CoreDepartmentApp/CoreDepartmentApp/Controllers/DepartController.cs
@@ -15,5 +15,29 @@ namespace CoreDepartmentApp.Controllers
             var degerler = context.departments.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public IActionResult NewDepartman()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewDepartman(department d)
+        {
+            context.departments.Add(d);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveDepartment(int id)
+        {
+            var dep = context.departments.Find(id);
+            context.departments.Remove(dep);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
