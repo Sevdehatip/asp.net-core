84e3a4876feebbd8761aa9fcea761f32cc15f016
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-02-09 23:07:23 +0300

    + employee controller

---
+++ CoreDepartmentApp/CoreDepartmentApp/Controllers/employeeController.cs
@@ -0,0 +1,19 @@
+﻿using CoreDepartmentApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDepartmentApp.Controllers
+{
+    public class employeeController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.employees.ToList();
+            return View(degerler);
+        }
+    }
+}
