93affb5b070b2bcf550dac424eda0405efa86062
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-02-14 17:01:41 +0300

    dropdownlist

--- CoreDepartmentApp/CoreDepartmentApp/Controllers/employeeController.cs
+++ CoreDepartmentApp/CoreDepartmentApp/Controllers/employeeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CoreDepartmentApp.Controllers
 {
@@ -16,5 +17,28 @@ namespace CoreDepartmentApp.Controllers
             var degerler = c.employees.Include(x => x.department).ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public IActionResult NewEmployee()
+        {
+            List<SelectListItem> value = (from x in c.departments.ToList()
+                                          select new SelectListItem
+                                          {
+                                              Text = x.departmentName,
+                                              Value = x.id.ToString()
+                                          }).ToList();
+            ViewBag.vl = value;
+            return View();
+        }
+
+        public IActionResult NewEmployee(employee e)
+        {
+            var per = c.departments.Where(x => x.id == e.department.id).FirstOrDefault();
+            e.department = per;
+            c.employees.Add(e);
+            c.SaveChanges();
+            return View("Index");
+        }
+
     }
 }
