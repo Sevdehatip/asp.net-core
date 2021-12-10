019b4fe5df3924821932f4903fa82573b301f1d4
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-12-10 22:47:59 +0300

    login page +

---
+++ CoreDepartmentApp/CoreDepartmentApp/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDepartmentApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
